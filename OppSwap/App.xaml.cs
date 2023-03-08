b36ad3bd657ad3e97474d744ce20f5081e0a7f87
Rajat Sengupta <stuntclub@outlook.com>
2023-03-08 12:08:27 -0800

    init

---
+++ OppSwap/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace OppSwap
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ OppSwap/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace OppSwap
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
