eeb38d2814b3a47ec5cb74873f6db25b53185f7e
Rajat Sengupta <stuntclub@outlook.com>
2023-03-09 12:06:48 -0800

    finished servercleint handshake with ping method

---
+++ OppSwap/ServerClient/Client.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+
+namespace OppSwap
+{
+    class Client
+    {
+        WebSocket ws;
+        String clientId;
+
+        public Client()
+        {
+            ws = new WebSocket("ws://localhost:9090");
+            ws.Connect();
+        }
+
+        public void Ping()
+        {
+            string s = @"{""method"": ""ping""}";
+            ws.Connect();
+            ws.Send(s);
+        }
+    }
+}
