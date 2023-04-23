5e0da44a48cfd415b0f69f48fd3b7dfa1b0a9621
s-skundurthi <s-skundurthi@lwsd.org>
2023-04-23 12:20:25 -0700

    Dynamic table view

--- OppSwap/ViewModels/JoinPageViewModel.cs
+++ OppSwap/ViewModels/JoinPageViewModel.cs
@@ -3,39 +3,41 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 namespace OppSwap.ViewModels
 {
-	public partial class JoinPageViewModel : ObservableObject
-	{
-		public JoinPageViewModel() {
+    public partial class JoinPageViewModel : ObservableObject
+    {
+        public JoinPageViewModel()
+        {
 
-		}
+        }
 
-		[ObservableProperty]
-		string gameCode;
+        [ObservableProperty]
+        string gameCode;
 
-		[RelayCommand]
-		void joinRoom()
-		{
-			if (string.IsNullOrWhiteSpace(GameCode)) {
-				return;
-			}
+        [RelayCommand]
+        void joinRoom()
+        {
+            if (string.IsNullOrWhiteSpace(GameCode))
+            {
+                return;
+            }
             ClientInterconnect.JoinGame(GameCode);
             GameCode = "";
-			//call raj's thing with the game code;
-			
-		}
+            //call raj's thing with the game code;
 
-		[RelayCommand]
+        }
+
+        [RelayCommand]
         async Task goToRoomDetailPage()
         {
-			ClientInterconnect.c.JoinGame(GameCode);
+            ClientInterconnect.c.JoinGame(GameCode);
 
-			await gameJoined(GameCode);
-			await Shell.Current.GoToAsync(nameof(RoomDetailPage),
-			new Dictionary<string, object>
-			{
-				//get the room we made with the textbox inside of it
-				["CurrRoom"] = ClientInterconnect.getRoom(GameCode)
-			});
+            await gameJoined(GameCode);
+            await Shell.Current.GoToAsync(nameof(RoomDetailPage),
+            new Dictionary<string, object>
+            {
+                //get the room we made with the textbox inside of it
+                ["CurrRoom"] = ClientInterconnect.getRoom(GameCode)
+            });
         }
         private static async Task gameJoined(String s)
         {
