046968a4ede98a852449e427ff6d1727f199da95
Vignesh Srinivasan <vigneshsrinivasan@Vigneshs-MacBook-Air.local>
2023-05-30 19:39:12 -0700

    ewrtyuj

--- OppSwap/ServerClient/Client.cs
+++ OppSwap/ServerClient/Client.cs
@@ -21,7 +21,7 @@ namespace OppSwap
         public List<String> errorMessages;
         public Client()
         {
-            ws = new WebSocket("ws://172.20.10.14:9992");//ws://water-cautious-barge.glitch.me");
+            ws = new WebSocket("ws://172.20.10.6:9992");//ws://water-cautious-barge.glitch.me");
             ws.Connect();
             ws.OnMessage += Ws_OnMessage;
 
